2a0d9b56ca4042b5161e17a05394d759f892b2dd
FanzeeApp <fanzeepubgm@gmail.com>
2024-11-06 03:22:13 +0500

    1

---
+++ main.js
@@ -0,0 +1,47 @@
+// // 2nchi
+
+// arr = [1, 2, 3, 4, 5, 6, 7, 8]
+
+// n = 3
+
+// array = []
+
+// for(i = 0; i < n; i++) {
+//    random = Math.floor(Math.random() * arr.length)
+//    array.push(random)
+// }
+
+// console.log(array)
+
+
+// // 3nchi
+
+// str = "apple banana apple orange banana"
+
+// function unSozlar(matn) {
+//     let sozlar = matn.split(" ");
+//     let unSozlar = [...new Set(sozlar)];
+//    return unSozlar;
+// }
+// let natija = unSozlar(str);
+// console.log(natija);
+
+
+// // 4nchi
+
+// let arr = [1, [2, 3], [4, [5, 6]]]
+// let natija = arr.flat(arr.length)
+// console.log(natija)
+
+// // 5nchi
+
+// str = "hello world from javascript"
+
+// javob = ""
+// arr = str.split(" ").forEach(item => {
+//    boshharf = item.charAt(0).toUpperCase()
+//    boshharf+= item.slice(1)
+//    javob += " " + boshharf
+// })
+
+// console.log(javob)
